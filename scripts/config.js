c41447e5e9fd2231f678267ad93c0851b17631f1
djbrout <djbrout@gmail.com>
2017-01-25 14:12:37 -0500

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,8 @@
 if(!window.App) window.App = {};
 App.Config = {
 	// The published spreadsheet ID
-	spreadSheetID: '14zox-ptRriyVgYz8796cIaZm52t1zvroZUTSczlRqbM',
+	spreadSheetID: '16DbAMcENw5R1qNGIMgJqN-LuzWj3k3g9ASugW-Ps_80',
 
 	// The public google drive folder ID
-	driveFolderID: '0B-wo61m8bE0Hd1pETGEwS05Wdmc'
+	driveFolderID: '0B2lZl-p7V8zGckxYQlRrcEdOOHc'
 }
